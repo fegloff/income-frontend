e8d5e91bc3e55cfda0f7782a96e11427e9a25678
fegloff <fegloff@gmail.com>
2022-04-14 13:19:38 -0500

    add metamask connection

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useWeb3React } from '@web3-react/core'
+import { injected } from "../../web3/connectors";
+import { Web3Provider } from '@ethersproject/providers';
+import { useEffect } from "react";
 
 const Header = () => {
+  
+  const { connector, error, activate, deactivate, active, chainId, account } = useWeb3React<Web3Provider>();
+  
+  const connect = async () => {
+    try {
+      await activate(injected);
+      console.log("CONNECT",active,chainId,error,account);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(()=>{
+    console.log("useefect");
+    console.log(active,chainId,error,account);
+  },[active,chainId,account]);
   return (
     <div>Hello
-      <button>Connect</button>
+      <button onClick={connect}>Connect</button>
     </div>
   )
 }
--- src/pages/earn/earn.component.tsx
+++ src/pages/earn/earn.component.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useWeb3React } from '@web3-react/core'
 
 const EarnPage = () => {
+  const { active, chainId, account } = useWeb3React();
+  console.log(active,chainId,account);
+
   return (
-    <div>EARN</div>
+    <div>
+      <h1>EARN</h1>
+      <div>Status: {active}</div>
+      <div>Account: {account}</div>
+      <div>Network ID: {chainId}</div>
+    </div>
   )
 }
 
