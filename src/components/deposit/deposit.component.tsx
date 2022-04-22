38b12750bbfd449acfbcd57223d277d751ce36b2
Freddy Polania <fpolania@gmail.com>
2022-04-22 08:42:41 -0400

    Input field in deposit component and styles refactor

--- src/components/deposit/deposit.component.tsx
+++ src/components/deposit/deposit.component.tsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import "./deposit.styles.scss";
 
-const Deposit: React.FC= () => {
-  const [title, setTitle] = useState('Your Total Deposit');
-  const [amount, setAmount] = useState(0.0);
+const Deposit: React.FC = () => {
+  const [title, setTitle] = useState("Your Total Deposit");
+  const [amount, setAmount] = useState<number>(0.0);
 
   return (
     <div className="deposit">
-    <div className="deposit__title">{title}</div>
-    <div className="deposit__total--small">{amount.toFixed(2)} ONE</div>
-    <div className="deposit__total--medium">{amount.toFixed(2)}</div>
-    <span className="deposit__total--warning">Not Enough!</span>
-    <div className="deposit__cta">
-      <button className="deposit__button button">Deposit</button>
-      <button className="deposit__button button">Withdraw</button>
+      <div className="deposit__title">{title}</div>
+      <div className="deposit__available">{amount.toFixed(2)} ONE</div>
+      <input
+        className="deposit__input"
+        type="number"
+        step="0.01"
+        placeholder="0.00"
+      ></input>
+      <span className="deposit__warning">Not Enough!</span>
+      <div className="deposit__cta">
+        <button className="deposit__button button">Deposit</button>
+        <button className="deposit__button button">Withdraw</button>
+      </div>
     </div>
-  </div>
   );
 };
 
